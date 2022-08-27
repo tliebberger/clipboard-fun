848c30fe96e286f18286dd92c588e37eecad34ef
Thomas Liebberger <thomas.liebberger@casavi.de>
2022-08-27 17:21:23 +0200

    event listener copy

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [c, setC] = useState("")
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handlCopy = event => {
+      setC(event.target.innerText)
+      navigator.clipboard.writeText("Random stuff injected by the site omega lol").then(() => {
+        console.log("hacked")
+      })
+
+    };
+
+    const element = ref.current;
+
+    element.addEventListener('copy', handlCopy);
+
+    return () => {
+      element.removeEventListener('copy', handlCopy);
+    };
+  }, []);
+
 
   useEffect(() => {
     const text = "Welcome to Clipboard Fun"
@@ -28,6 +49,21 @@ export default function Home() {
         <p className={styles.description}>
           This is a simple app that will copy the text Welcome to Clipboard Fun to the clipboard. Without asking you for permission.
         </p>
+        <br />
+        <span ref={ref}>Now copy this line and check your clipboard</span>
+        <br />
+        {c.length > 0 &&
+          <>
+            <div>
+              <p>you have copied:</p>
+              <p>{c}</p>
+              <input
+                style={{ width: '100%' }}
+                placeholder='copy your clipboard in here'
+              />
+
+            </div>
+          </>}
       </main>
     </div>
   )
